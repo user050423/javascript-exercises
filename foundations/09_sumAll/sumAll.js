bdccf52aba883a3119045c7453a56f29e7fa9d6f
Ynotkokonut <naijabami@gmail.com>
2026-03-10 10:19:07 +0800

    Sum all numbers between two numbers

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number' || !Number.isInteger(num1) || !Number.isInteger(num2)) return 'ERROR';    for (let i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
