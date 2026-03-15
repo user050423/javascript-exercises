beeffdfecabc4b7800fb6da4c0b158c044d7ee20
Ynotkokonut <naijabami@gmail.com>
2026-03-16 00:58:17 +0800

    check if a string is a palindrome

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    const cleanStr = str.replace(/[^0-9a-zA-Z]/g, '').toLowerCase();
+    // Check if the cleaned string is the same as its reverse
+    const reversedStr = cleanStr.split('').reverse().join('');
+    return cleanStr === reversedStr;
 };
 
 // Do not edit below this line
