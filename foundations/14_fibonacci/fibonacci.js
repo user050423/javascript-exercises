190261a90be453a4ece1c19ab557b4ca9712f648
Ynotkokonut <naijabami@gmail.com>
2026-03-16 15:00:20 +0800

    change to algorithm to use recursion

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,16 +1,9 @@
-const fibonacci = function(num) {
-    if (num < 0) return "OOPS";
-    if (num === 0) return 0;
-    if (num === 1) return 1;
-
-    let a = 0, b = 1, c;
-    for (let i = 2; i <= num; i++) {
-        c = a + b;
-        a = b;
-        b = c;
-    }
-    return c;
-};
+function fibonacci(n) {
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
